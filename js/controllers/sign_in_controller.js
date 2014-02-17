b171f0c0cae758d5b91f0ba709f84d47a17248f1
Ruslan Yakhyaev <ruslan@ruslan.io>
2014-02-17 14:01:49 +0100

    Chapter 8: Sign in action

--- js/controllers/sign_in_controller.js
+++ js/controllers/sign_in_controller.js
@@ -1,2 +1,9 @@
 App.SignInController = Ember.Controller.extend({
+  actions: {
+    signIn: function() {
+      var email = this.get('email');
+      userToLogin = App.User.FIXTURES.findBy('email', email);
+      localStorage['currentUser'] = userToLogin.id;
+    }
+  }
 });
